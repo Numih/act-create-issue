5d85387aa630e28c69d96ec61e9ff3f136aa8ff1
numih-g <g@numih.com>
2022-08-04 14:08:47 +0000

    Create issue action

---
+++ index.js
@@ -0,0 +1,18 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run(){
+
+    const octokit = github.getOctokit(core.getInput('token'));
+    const context = github.context;
+
+    await octokit.request('POST /repos/{owner}/{repo}/issues', {
+        owner: context.repo.owner,
+        repo: core.getInput('repo', { required: true }),
+        title: core.getInput('title', { required: true }),
+        body: core.getInput('body', { required: true }),
+        labels: core.getInput('labels').split(',')
+    })
+}
+
+run();
